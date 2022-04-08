8686d50415bbea8ac354f322e4934b76a3267658
dute0987 <dute0987@gmail.com>
2022-04-08 18:00:16 +0530

    Vishal Dute- program for emplyoee wage stofre value in array

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -9,3 +9,42 @@ else
 {
     console.log("PRESENT");
 }
+
+//UC6 Store Wage in Array
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NO_OF_WORKING_DAYS = 20;
+const MAX_WORKING_HR = 160;
+function getWorkingHour(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+
+let totalHrs = 0;
+let totalWorkingdays=0;
+let empDailyWageArr = new Array();
+
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
+
+while( totalHrs <= MAX_WORKING_HR && totalWorkingdays < NO_OF_WORKING_DAYS){
+    totalWorkingdays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalHrs += getWorkingHour(empCheck);
+    empHrs = getWorkingHour(empCheck);
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+let empWage = calcDailyWage(totalHrs);
+console.log(" Total days "+totalWorkingdays+" Hours worked "+totalHrs+" Emp wage: "+empWage);
