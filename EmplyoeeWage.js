a677f6e1873e76dc80a70582fad85b45e0418cf4
dute0987 <dute0987@gmail.com>
2022-04-08 18:37:13 +0530

    Vishal Dute- Program for Employee Payroll using Regex Expression

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -1,11 +1,47 @@
-//Welcome to Emplyoee Wage Problem using JavaScript
+//UC11 Employee payroll class
+class EmployeePayrollData {
+    //property
+    id;
+    salary;
+    gender;
+    startDate;
 
-const IsABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10)%2;
-if(empCheck == IsABSENT){
-    console.log("ABSENT");
+    //constructor
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    //getter and setter
+    get name() { return this._name;}
+    set name(name) {
+        let nameRegex = new RegExp("^[A-Z]{1}[a-z]{3,}$");
+        if( nameRegex.test(name)){
+            this._name = name;
+        }
+        else{
+            throw "Name is incorrect format";
+        }
+    }        
+
+    //method
+    toString(){
+        const option = { year: 'numeric', month : 'long', day : 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US",option);
+        return "id ="+this.id+", name = "+this.name+", salary = "+this.salary+", gender = "+this.gender+", startDate "+empDate;
+    }
 }
-else
-{
-    console.log("PRESENT");
+
+let employeePayrollData = new EmployeePayrollData(1,"Vishal",50000);
+console.log(employeePayrollData.toString());
+try{
+    employeePayrollData.name = "john";
+    console.log(employeePayrollData.toString());
+}catch(e){
+    console.error(e);
 }
+let newemployeePayrollData = new EmployeePayrollData(2,"Terissa",70000,"Female",new Date());
+console.log(newemployeePayrollData.toString());
