a35998b9f65fe19e3bbbb654922468213d57f276
dute0987 <dute0987@gmail.com>
2022-04-08 17:34:21 +0530

    Vishal Dute- Greeting message for Emplyoee wage problem

---
+++ EmplyoeeWage.js
@@ -0,0 +1,11 @@
+//Welcome to Emplyoee Wage Problem using JavaScript
+
+const IsABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10)%2;
+if(empCheck == IsABSENT){
+    console.log("ABSENT");
+}
+else
+{
+    console.log("PRESENT");
+}
