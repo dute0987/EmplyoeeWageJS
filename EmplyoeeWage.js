6c2333a890846b9ea86092b52e11436a93d95461
dute0987 <dute0987@gmail.com>
2022-04-08 17:55:40 +0530

    Vishal Dute- program for emplyoee wage to calculate wage till condition satisfied

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -2,10 +2,42 @@
 
 const IsABSENT = 0;
 let empCheck = Math.floor(Math.random() * 10)%2;
-if(empCheck == IsABSENT){
+if(empCheck == IsABSENT)
+{
     console.log("ABSENT");
 }
 else
 {
     console.log("PRESENT");
 }
+
+//UC5 Calculation of wage tillcondition
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NO_OF_WORKING_DAYS = 20;
+const MAX_WORKING_HR = 160;
+function getWorkingHour(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+
+let totalHrs = 0;
+let totalWorkingdays=0;
+while( totalHrs <= MAX_WORKING_HR && totalWorkingdays < NO_OF_WORKING_DAYS){
+    totalWorkingdays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalHrs += getWorkingHour(empCheck);
+}
+let empWage = totalHrs * WAGE_PER_HOUR;
+console.log(" Total days "+totalWorkingdays+" Hours worked "+totalHrs+" Emp wage: "+empWage);
