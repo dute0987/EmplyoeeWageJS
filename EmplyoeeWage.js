ece89e7e7cb66c1a6abdf04491d47cab7475a40f
dute0987 <dute0987@gmail.com>
2022-04-08 18:33:12 +0530

    Vishal Dute- Program for Employee Payroll Class extension

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -1,11 +1,35 @@
-//Welcome to Emplyoee Wage Problem using JavaScript
+//UC11 Employee payroll class
+class EmployeePayrollData {
+    //property
+    id;
+    salary;
+    gender;
+    startDate;
 
-const IsABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10)%2;
-if(empCheck == IsABSENT){
-    console.log("ABSENT");
+    //constructor
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    //getter and setter
+    get name() { return this._name;}
+    set name(name) {this._name = name;}
+
+    //method
+    toString(){
+        const option = { year: 'numeric', month : 'long', day : 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US",option);
+        return "id ="+this.id+", name = "+this.name+", salary = "+this.salary+", gender = "+this.gender+", startDate "+empDate;
+    }
 }
-else
-{
-    console.log("PRESENT");
-}
+
+let employeePayrollData = new EmployeePayrollData(1,"Kalpesh",50000);
+console.log(employeePayrollData.toString());
+employeePayrollData.name = "John";
+console.log(employeePayrollData.toString());
+let newemployeePayrollData = new EmployeePayrollData(2,"Terissa",70000,"Female",new Date());
+console.log(newemployeePayrollData.toString());
